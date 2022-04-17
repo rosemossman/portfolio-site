c63aa87bb41becaefecfb9e44ca56f69bef7218b
rosemossman <102536369+rosemossman@users.noreply.github.com>
2022-04-17 14:26:09 -0400

    add hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Hi, Welcome to my Portfolio Website!")
+console.log("Hi, Welcome to my Portfolio Website!");
+
+function menuToggle()  {
+    var x = document.getElementById('myNavtoggle'); 
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+   }
+
